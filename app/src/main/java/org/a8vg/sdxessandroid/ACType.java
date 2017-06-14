55592f419fae33eb27f00a9e47c0e3820aa7ce3f
carlos <carlos.sanchez@squadventure.com>
2017-06-14 21:18:07 +0800

    Initial commit

---
+++ app/src/main/java/org/a8vg/sdxessandroid/ACType.java
@@ -0,0 +1,13 @@
+package org.a8vg.sdxessandroid;
+
+/**
+ * Created by kotaro on 6/13/2017.
+ */
+
+public enum ACType {
+    NOTYPE,
+    STARTER,
+    BASIC,
+    ADVANCED
+}
+
---
+++ app/src/main/java/org/a8vg/sdxessandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package org.a8vg.sdxessandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
